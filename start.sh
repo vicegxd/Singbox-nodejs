1b8563ea5d66caa151388d810310c532ebf7a6a6
vicegxd <34847052+vicegxd@users.noreply.github.com>
2026-01-03 22:02:40 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 set -e
 
 # ================== 端口设置 ==================
-export TUIC_PORT=${TUIC_PORT:-"8000"}
+export TUIC_PORT=${TUIC_PORT:-"80"}
 export HY2_PORT=${HY2_PORT:-""}
-export REALITY_PORT=${REALITY_PORT:-"8000"}
+export REALITY_PORT=${REALITY_PORT:-"80"}
 
 # ================== 强制切换到脚本所在目录 ==================
 cd "$(dirname "$0")"
