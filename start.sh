476f35ae40fc1f3679a27261cb0723b317adbc9a
vicegxd <34847052+vicegxd@users.noreply.github.com>
2026-01-03 22:33:59 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 # ================== 端口设置 ==================
-export TUIC_PORT=${TUIC_PORT:-"22"}
+export TUIC_PORT=${TUIC_PORT:-"3000"}
 export HY2_PORT=${HY2_PORT:-""}
 export REALITY_PORT=${REALITY_PORT:-"3000"}
 
